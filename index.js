cf6448d2f0b34fddab3edc3d7e7f7749916cf67b
BaadBadu Beats <baadbadubeatss@gmail.com>
2022-07-04 22:03:52 -0300

    nou pase etap variable yo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello Bastard kkkkkk");
+var unTexte = "eple baadbadu";
+
+console.log(unTexte);
+
+let bbb = "badu beats li modifyab";
+
+console.log(bbb);
+
+const bbb2 = "badu beats l'ui pa modifyab";
+
+console.log(bbb2);
+
+/*********** ou dwe mete sa \ devan ' siw utilize yon seul pou var */
