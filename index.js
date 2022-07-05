74d88527e1091cc9bad6a3c6e8d9e81062ededd2
BaadBadu Beats <baadbadubeatss@gmail.com>
2022-07-04 22:18:50 -0300

    nou aprann adisyone variab anndan lot ak LET

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ const bbb2 = "badu beats l'ui pa modifyab";
 console.log(bbb2);
 
 /*********** ou dwe mete sa \ devan ' siw utilize yon seul pou var */
+
+/********CONCATENATION */
+
+let chif = "50";
+
+let ane = "ki laj ti tom :" + chif + " an";
+
+console.log(ane);
